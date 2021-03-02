0e4dd7d29b388384de577d5ef2998112c7ff4e6b
Mungai <mungaihpk@gmail.com>
2021-03-02 15:56:00 +0300

    Initial commit, project setup and ReadMe file. The libraries are included in the project. use eclipse for seamless import/integration.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Britam-FAP
---
+++ src/variables/Urls.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Urls {
+	final private String login_url = "https://10.10.98.50/web/financial-advisor-portal/login?p_p_id=com_liferay_login_web_portlet_LoginPortlet&p_p_lifecycle=0&_com_liferay_login_web_portlet_LoginPortlet_redirect=%2F";
+			
+	public Urls(){
+		
+	}
+	
+	public String getLoginUrl() {
+		return login_url;
+	}
+}
