8f8e3b375b48e9e7d12daac8286e6b8f81a28e1f
Mungai <mungaihpk@gmail.com>
2021-03-05 11:02:53 +0300

    Added logic to test for login and proposal retrieval

--- src/variables/Urls.java
+++ src/variables/Urls.java
@@ -2,6 +2,7 @@ package variables;
 
 public class Urls {
 	final private String login_url = "https://10.10.98.50/web/financial-advisor-portal/login?p_p_id=com_liferay_login_web_portlet_LoginPortlet&p_p_lifecycle=0&_com_liferay_login_web_portlet_LoginPortlet_redirect=%2F";
+	final private String faconnect_url = "https://faconnectuat.britam.com";
 			
 	public Urls(){
 		
@@ -9,5 +10,9 @@ public class Urls {
 	
 	public String getLoginUrl() {
 		return login_url;
+	}
+	
+	public String getFaConnectUrl() {
+		return faconnect_url;
 	}
 }
